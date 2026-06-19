c728bd933b18f9bc6ba614cf711c63c501cbfade
Ankit Yadav <ankit.runtime@gmail.com>
2026-06-19 12:50:10 +0530

    adding menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link  } from "react-router-dom";
 
 import About from "./Pages/About";
 import Services from "./Pages/Services";
@@ -7,12 +7,18 @@ import Home from "./Pages/Home1";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
+      <nav>
+        <ul style={{ display: "flex", gap: "20px", listStyle: "none" }}>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/services">Services</Link></li>
+        </ul>
+      </nav>
 
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
-
       </Routes>
     </BrowserRouter>
   );

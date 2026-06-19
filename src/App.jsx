1ed7696a725d6cbc6c3da6402e2f62220d656266
Ankit Yadav <ankit.runtime@gmail.com>
2026-06-19 11:48:41 +0530

    Fix import path casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import About from "./pages/About";
+import About from "./Pages/About";
 import Services from "./Pages/Services";
 import Home from "./Pages/Home";
 
